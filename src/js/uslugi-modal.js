01cbeb2cebd0ac2b04546b6aa4d1cccca346e69f
Pavlo Gurskiy <pavlo.gurskiy@gmail.com>
2025-04-15 05:54:20 +0300

    exp-9

--- src/js/uslugi-modal.js
+++ src/js/uslugi-modal.js
@@ -5,16 +5,16 @@ const closeModal = document.querySelector('.modal-close');
 
 anchors.forEach(anchor => {
   anchor.addEventListener('click', function (e) {
-    e.preventDefault(); // зупинити перехід по посиланню
-    const imgSrc = this.getAttribute('data-img'); // беремо адресу картинки
+    e.preventDefault(); // Зупинити перехід по посиланню
+    const imgSrc = this.getAttribute('data-img'); // Беремо адресу картинки
 
     // Створюємо новий об'єкт Image
     const img = new Image();
 
     // Додаємо обробник події завантаження
     img.onload = function () {
-      modal.style.display = 'block'; // Відкриваємо модалку тільки після завантаження картинки
       modalImg.src = imgSrc; // Встановлюємо src зображення для модалки
+      modal.style.display = 'block'; // Відкриваємо модалку після завантаження картинки
     };
 
     img.onerror = function () {
@@ -30,7 +30,7 @@ closeModal.addEventListener('click', function () {
   modal.style.display = 'none';
 });
 
-// закривати модалку при кліку поза картинкою
+// Закривати модалку при кліку поза картинкою
 modal.addEventListener('click', function (e) {
   if (e.target === modal) {
     modal.style.display = 'none';
