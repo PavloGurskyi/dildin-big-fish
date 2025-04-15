21c6b8a796dada0c8656f8a40693762ccd2c2ae7
Pavlo Gurskiy <pavlo.gurskiy@gmail.com>
2025-04-15 06:04:26 +0300

    exp-10

--- src/js/uslugi-modal.js
+++ src/js/uslugi-modal.js
@@ -1,38 +1,29 @@
-const anchors = document.querySelectorAll('.serv-nav-item-anchor');
-const modal = document.getElementById('modal');
-const modalImg = document.getElementById('modal-img');
-const closeModal = document.querySelector('.modal-close');
+// Оберігаємо функціональність до завантаження DOM
+document.addEventListener('DOMContentLoaded', function () {
+  const modal = document.getElementById('modal');
+  const modalImg = document.getElementById('modal-img');
+  const closeBtn = document.querySelector('.modal-close');
+  const anchors = document.querySelectorAll('.serv-nav-item-anchor');
 
-anchors.forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault(); // Зупинити перехід по посиланню
-    const imgSrc = this.getAttribute('data-img'); // Беремо адресу картинки
-
-    // Створюємо новий об'єкт Image
-    const img = new Image();
-
-    // Додаємо обробник події завантаження
-    img.onload = function () {
-      modalImg.src = imgSrc; // Встановлюємо src зображення для модалки
-      modal.style.display = 'block'; // Відкриваємо модалку після завантаження картинки
-    };
-
-    img.onerror = function () {
-      console.error('Помилка завантаження зображення');
-    };
-
-    // Встановлюємо шлях до картинки в src об'єкта Image для її завантаження
-    img.src = imgSrc;
+  // Відкриття модального вікна
+  anchors.forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const imgSrc = anchor.getAttribute('data-img');
+      modal.style.display = 'flex';
+      modalImg.src = imgSrc;
+    });
   });
-});
 
-closeModal.addEventListener('click', function () {
-  modal.style.display = 'none';
-});
-
-// Закривати модалку при кліку поза картинкою
-modal.addEventListener('click', function (e) {
-  if (e.target === modal) {
+  // Закриття модального вікна
+  closeBtn.addEventListener('click', function () {
     modal.style.display = 'none';
-  }
+  });
+
+  // Закриття модального вікна при натисканні поза межами зображення
+  window.addEventListener('click', function (e) {
+    if (e.target === modal) {
+      modal.style.display = 'none';
+    }
+  });
 });
